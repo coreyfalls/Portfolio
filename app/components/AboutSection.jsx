ece6914438a6f7bb307bda7ad0c10bfd702aa46c
Cyberjutsu <cyb3rjutsu@gmail.com>
2024-02-01 10:11:43 -0600

    fix

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -53,11 +53,21 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/image/peace-cloud.png" alt="masked man holding up peace sign behind computer"width={500} height={500} />
+        <Image
+          src="/image/peace-cloud.png"
+          alt="masked man holding up peace sign behind computer"
+          width={500}
+          height={500}
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          As an individual with a knack for creativity, leadership, organization, problem-solving, and teamwork, I bring a unique blend of skills to the table. I am excited about the ongoing journey from developer to cybersecurity enthusiast, with a clear goal of becoming a Cloud Security Engineer. Let's connect and explore how my skills and experiences can contribute to the success of your team!
+            As an individual with a knack for creativity, leadership,
+            organization, problem-solving, and teamwork, I bring a unique blend
+            of skills to the table. I am excited about the ongoing journey from
+            developer to cybersecurity enthusiast, with a clear goal of becoming
+            a Cloud Security Engineer. Let's connect and explore how my skills
+            and experiences can contribute to the success of your team!
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
