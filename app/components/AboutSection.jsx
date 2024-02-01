10e689e48a9332e83ec3def899acc2d188606943
Cyberjutsu <cyb3rjutsu@gmail.com>
2024-02-01 09:38:45 -0600

    quick fixes for a better look

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -1,8 +1,9 @@
-"use client";
+// Importing necessary dependencies
 import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+// Data for each tab
 const TAB_DATA = [
   {
     title: "Skills",
@@ -40,48 +41,47 @@ const TAB_DATA = [
   },
 ];
 
+// AboutSection component
 const AboutSection = () => {
+  // State for selected tab and transition
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
 
+  // Function to handle tab changes
   const handleTabChange = (id) => {
     startTransition(() => {
       setTab(id);
     });
   };
 
+  // JSX for the AboutSection component
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        {/* Image component */}
         <Image src="/image/peace-cloud.png" width={500} height={500} />
+
+        {/* About Me content */}
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          As an individual with a knack for creativity, leadership, organization, problem-solving, and teamwork, I bring a unique blend of skills to the table. I am excited about the ongoing journey from developer to cybersecurity enthusiast, with a clear goal of becoming a Cloud Security Engineer. Let's connect and explore how my skills and experiences can contribute to the success of your team!
+            As an individual with a knack for creativity, leadership, organization, problem-solving, and teamwork, I bring a unique blend of skills to the table. I am excited about the ongoing journey from developer to cybersecurity enthusiast, with a clear goal of becoming a Cloud Security Engineer. Let's connect and explore how my skills and experiences can contribute to the success of your team!
           </p>
+
+          {/* Tab buttons for Skills, Education, and Certifications */}
           <div className="flex flex-row justify-start mt-8">
-            <TabButton
-              selectTab={() => handleTabChange("skills")}
-              active={tab === "skills"}
-            >
-              {" "}
-              Skills{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("education")}
-              active={tab === "education"}
-            >
-              {" "}
-              Education{" "}
-            </TabButton>
-            <TabButton
-              selectTab={() => handleTabChange("certifications")}
-              active={tab === "certifications"}
-            >
-              {" "}
-              Certifications{" "}
-            </TabButton>
+            {TAB_DATA.map((item) => (
+              <TabButton
+                key={item.id}
+                selectTab={() => handleTabChange(item.id)}
+                active={tab === item.id}
+              >
+                {item.title}
+              </TabButton>
+            ))}
           </div>
+
+          {/* Content based on selected tab */}
           <div className="mt-8">
             {TAB_DATA.find((t) => t.id === tab).content}
           </div>
@@ -91,4 +91,5 @@ const AboutSection = () => {
   );
 };
 
+// Exporting the AboutSection component
 export default AboutSection;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -18,31 +18,26 @@ const navLinks = [
 
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
-        <Link
-          href={"/"}
-          className="text-2xl md:text-3xl text-white font-semibold"
-        >
-          Home
+        <Link href="/" passHref>
+          <a className="text-2xl md:text-3xl text-white font-semibold">Home</a>
         </Link>
         <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
-            >
+          <button
+            onClick={() => setNavbarOpen(!navbarOpen)}
+            className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+            aria-expanded={navbarOpen}
+            aria-controls="navbar"
+          >
+            {navbarOpen ? (
               <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
+            ) : (
+              <Bars3Icon className="h-5 w-5" />
+            )}
+          </button>
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
@@ -54,7 +49,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
-      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+      {navbarOpen && <MenuOverlay links={navLinks} />}
     </nav>
   );
 };
