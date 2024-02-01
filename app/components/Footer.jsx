39d935b7c49b6984aff1d142778c87bd4553a2c6
Cyberjutsu <cyb3rjutsu@gmail.com>
2024-02-01 09:42:15 -0600

    fixed breakage

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white py-8">
-      <div className="container flex flex-col md:flex-row items-center justify-between">
-        <address className="text-sm md:mr-8">Have a nice day, stay vigilant!</address>
-        <p className="text-sm text-slate-600 mt-4 md:mt-0">© {new Date().getFullYear()} Your Company. All rights reserved.</p>
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>Have a nice day, stay vigilant!</span>
+        <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
   );
