e672e59f05de1f9f014b2914f42e349904c16d67
Cyberjutsu <cyb3rjutsu@gmail.com>
2024-02-01 10:01:42 -0600

    fixed alt issue

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
           <div className="rounded-full bg-[#181818] w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/image/cloud-pi.png"
-              alt="cloud sec image"
+              alt="masked man security logo"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
